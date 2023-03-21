2b310b8f26ca4c13a9777f6c98a0276cc197c048
JereLem <56977360+JereLem@users.noreply.github.com>
2023-03-21 17:06:45 +0200

    4.9-4.10

--- blogisovellus/tests/blog_api.test.js
+++ blogisovellus/tests/blog_api.test.js
@@ -12,6 +12,7 @@ test('blogs are returned as json', async () => {
     .expect('Content-Type', /application\/json/)
 })
 
+// Test that blogs are returned
 describe('blogs api', () => {
     test('blogs are returned as JSON', async () => {
       const res = await api.get('/api/blogs');
@@ -24,6 +25,47 @@ describe('blogs api', () => {
       expect(res.body.length).toEqual(listHelper.listWithMultipleBlogs.length);
     });
   });
+
+
+// Test that posted blogs have the correct id
+describe('returned blog has an id field', () => {
+    test('the id field is defined', async () => {
+      const res = await api.get('/api/blogs')
+      const blogs = res.body
+      blogs.forEach(blog => {
+        expect(blog.id).toBeDefined()
+      })
+    })
+  })
+
+
+// Test that new blog are added correctly
+describe('adding a new blog', () => {
+    test('Good, valid data!', async () => {
+      const newBlog = {
+        title: 'Test',
+        author: 'Test',
+        url: 'http://testurl.com',
+        likes: 123
+      }
+
+  
+      await api
+        .post('/api/blogs')
+        .send(newBlog)
+        .expect(201)
+        .expect('Content-Type', /application\/json/)
+  
+      const res = await api.get('/api/blogs')
+      const blogs = res.body
+  
+      expect(blogs).toHaveLength(blogs.length)
+  
+      const titles = blogs.map(blog => blog.title)
+      expect(titles[titles.length-1]).toEqual("Test")
+    })
+  })
+  
 
 afterAll(async () => {
   await mongoose.connection.close()
